612b5091dc78fc9252c32b3526e8aaaa2177b4a3
Allen Perl <allen@route.com>
2020-01-14 13:35:09 -0700

    update comments

--- main.go
+++ main.go
@@ -13,9 +13,28 @@ var emptyBody = make([]byte, 0)
 var emptyString = ""
 
 func printHelp() {
-	println("Use: rims <PORT>")
-	println("To set the mock response POST to host:PORT/mock")
-	println("The Content-Type in the post will be returned in the body of the next request")
+	println(`
+Use: rims <PORT>
+
+Setup:
+	To setup data to be mocked post it as the body to http://host:PORT/mock
+Requests:
+	All requests sent to the mock server will respond with the values the values
+	posted to /mock.
+
+
+Response Body:
+	Whatever body that is posted to /mock with be the body of all other responses.
+Content-Type:
+	Whatever content-type header is present on the post to /mock will be the
+	content-type on all other responses
+Status Code:
+	The status code of a request by default will be 200. If another status code is
+	desired then the status code can be set as a query param on the post to /mock.
+
+	EX: /mock?status=404
+
+`)
 }
 
 func main() {
